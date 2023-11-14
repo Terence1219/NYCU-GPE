9da07d7fec6103bc6510a6ab9e9de1298bf6c5a5
Terence1219 <terence1219@gmail.com>
2023-11-14 17:21:51 +0800

    add 10655-Sumsets

---
+++ 10655-Sumsets/10655-Sumsets.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int find(vector<int> set)
+{
+    for (int d = set.size() - 1; d >= 0; d--)
+    {
+        for (int a = 0; a < set.size(); a++)
+        {
+            for (int b = a + 1; b < set.size(); b++)
+            {
+                for (int c = b + 1; c < set.size(); c++)
+                {
+                    if (set[a] + set[b] + set[c] == set[d])
+                        return set[d];
+                }
+            }
+        }
+    }
+    return INT_MAX;
+};
+
+int main()
+{
+    int n;
+    while (cin >> n && n != 0)
+    {
+        vector<int> set;
+        while (n)
+        {
+            int element;
+            cin >> element;
+            set.push_back(element);
+            n--;
+        }
+        sort(set.begin(), set.end());
+        int rtn = find(set);
+        if (rtn == INT_MAX)
+            cout << "no solution";
+        else
+            cout << rtn;
+    }
+
+    return 0;
+}
